9969e4a4f371ee74b8ff04889116398bb8331be6
BRajendra10 <rajendrabehera8116@gmail.com>
2025-06-02 19:27:20 +0530

    Setup db.json with package.json

--- main.js
+++ main.js
@@ -21,16 +21,23 @@ prev.addEventListener("click", () => {
 });
 
 // accordion
- document.querySelectorAll('.accordion-toggle').forEach(button => {
-    button.addEventListener('click', () => {
-      const content = button.nextElementSibling;
-      const icon = button.querySelector('.arrow-icon');
+document.querySelectorAll(".accordion-toggle").forEach((button) => {
+  button.addEventListener("click", () => {
+    const content = button.nextElementSibling;
+    const icon = button.querySelector(".arrow-icon");
 
-      const isOpen = !content.classList.contains('hidden');
-      content.classList.toggle('hidden');
+    const isOpen = !content.classList.contains("hidden");
+    content.classList.toggle("hidden");
 
-      // Swap icon class
-      icon.classList.toggle('ri-arrow-down-s-line', isOpen);
-      icon.classList.toggle('ri-arrow-up-s-line', !isOpen);
-    });
+    // Swap icon class
+    icon.classList.toggle("ri-arrow-down-s-line", isOpen);
+    icon.classList.toggle("ri-arrow-up-s-line", !isOpen);
   });
+});
+
+(function () {
+  fetch("http://localhost:3000/info")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+})();
