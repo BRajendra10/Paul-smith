f4525f8a7f2b9774f32f1b44e20cd87a556b39d6
BRajendra10 <rajendrabehera8116@gmail.com>
2025-06-05 23:11:08 +0530

    commits

--- script/discription.js
+++ script/discription.js
@@ -24,29 +24,19 @@ async function get_data() {
   document.getElementById("img-1").src = data.images[0].url
   document.getElementById("img-2").src = data.images[1].url
 
-  console.log(data.images[0].url)
   map_image(data.images);
 }
 
 get_data();
 
 function map_image(images) {
-  const image_list = images.map((el, i) => {
-    let tag = "img";
+  const image_list = images.map((el, i) => create_element("img", ["w-full", "h-auto", "rounded"], el))
 
-    if(el.type === "video") tag = "video"
-
-    const img_vedio = create_element(tag, ["w-full", "h-auto", "rounded"], el.url);
+  image_list.forEach((img) =>{
     const img_container = create_element("div", ["flex-shrink-0", "w-[500px]", "lg:w-[620px]"]);
-
-    img_container.appendChild(img_vedio);
-
-    return img_container;
-  });
-
-  image_list.forEach((img_container) => {
+    img_container.appendChild(img)
     carousel.appendChild(img_container);
-  });
+  })
 }
 
 function create_element(tag, class_name = [], tag_info){
@@ -55,13 +45,6 @@ function create_element(tag, class_name = [], tag_info){
   class_name.forEach((cls) => el.classList.add(cls))
 
   if (tag_info !== undefined) el.innerHTML = tag_info;
-
-  if(tag === "vedio") {
-    img_vedio.setAttribute("autoplay", "");
-    img_vedio.setAttribute("loop", "");
-    img_vedio.setAttribute("muted", "");
-    img_vedio.setAttribute("playsInline", "");
-  }
 
   return el;
 }
--- script/product.js
+++ script/product.js
@@ -1,7 +1,7 @@
 const products_container = document.getElementById("products-container");
 
 async function get_data() {
-  const res = await fetch("http://localhost:3000/discription");
+  const res = await fetch("http://localhost:3000/product");
   const data = await res.json();
 
   console.log(data)
@@ -39,10 +39,10 @@ function product(product_data) {
     <div class="w-full mt-2 px-1">
       <div class="flex justify-between items-start flex-wrap gap-y-1">
         <h4 class="w-[75%] text-sm sm:text-xs uppercase font-semibold cursor-pointer" data-id="${product_data.id}">
-          ${product_data.title}
+          ${product_data.name}
         </h4>
         <p class="text-sm sm:text-xs font-semibold">
-          ₹${product_data.price.amount}
+          ${product_data.price}
         </p>
       </div>
 
