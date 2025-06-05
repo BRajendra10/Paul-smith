e964e681bfa960806c42bf1419dad33105548633
BRajendra10 <rajendrabehera8116@gmail.com>
2025-06-05 18:47:27 +0530

    added image in discription dynamically tempraryly

--- script/discription.js
+++ script/discription.js
@@ -11,41 +11,25 @@ prevBtn.addEventListener("click", () => {
 });
 
 async function get_data() {
-  const res = await fetch("http://localhost:3000/discription/2");
+  const res = await fetch("http://localhost:3000/discription/4");
   const data = await res.json();
 
-  map_data(data.images);
+  map_image(data.images);
 
   // const img_container = create_element("div", ["flex-shrink-0", "w-[500px]","lg:w-[620px]"])
 }
 get_data();
 
-function map_data(images) {
+function map_image(images) {
   const image_list = images.map((el, i) => {
-    let img_vedio = "";
+    let tag = "img";
 
-    if (el.type === "video") {
-      img_vedio = create_image("video", el.url, ["w-full", "h-auto", "rounded"])
+    if(el.type === "video") tag = "video"
 
-        img_vedio.setAttribute("autoplay", "");
-        img_vedio.setAttribute("loop", "");
-        img_vedio.setAttribute("muted", "");
-        img_vedio.setAttribute("playsinline", "");
-    } else {
-      tag = img_vedio = create_image("img", el.url, ["w-full", "h-auto", "rounded"]);
-    }
-
-    // const img = create_image(tag, el.url, ["w-full", "h-auto", "rounded"]);
-    // console.log(img);
-
-    const img_container = create_element("div", [
-      "flex-shrink-0",
-      "w-[500px]",
-      "lg:w-[620px]",
-    ]);
+    const img_vedio = create_element(tag, ["w-full", "h-auto", "rounded"], el.url);
+    const img_container = create_element("div", ["flex-shrink-0", "w-[500px]", "lg:w-[620px]"]);
 
     img_container.appendChild(img_vedio);
-    console.log(img_container);
 
     return img_container;
   });
@@ -54,22 +38,21 @@ function map_data(images) {
     carousel.appendChild(img_container);
   });
 
-  // carousel.innerHTML = image_list
 }
 
-function create_image(eliment, source, class_name = []) {
-  let el = document.createElement(eliment);
-  el.src = source;
-
-  class_name.forEach((cls) => el.classList.add(cls));
-
-  return el;
-}
-
-function create_element(tag, class_name = [], tag_info) {
+function create_element(tag, class_name = [], tag_info){
   let el = document.createElement(tag);
-  class_name.forEach((cls) => el.classList.add(cls));
+  el.src = tag_info;
+  class_name.forEach((cls) => el.classList.add(cls))
+
   if (tag_info !== undefined) el.innerHTML = tag_info;
+
+  if(tag === "vedio") {
+    img_vedio.setAttribute("autoplay", "");
+    img_vedio.setAttribute("loop", "");
+    img_vedio.setAttribute("muted", "");
+    img_vedio.setAttribute("playsInline", "");
+  }
 
   return el;
 }
