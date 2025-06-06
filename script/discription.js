5cc8f29a785f16166089c64dda43519aa655f4a4
BRajendra10 <rajendrabehera8116@gmail.com>
2025-06-06 20:43:37 +0530

    modify discription.js and .html

--- script/discription.js
+++ script/discription.js
@@ -13,10 +13,73 @@ prevBtn.addEventListener("click", () => {
 const z = new URLSearchParams(window.location.search);
 const id = z.get("id");
 const data = await get_data();
+map_image(data);
 
-document.getElementById("title").innerHTML = data.title;
-document.getElementById("price").innerHTML = `$${data.price}`;
-document.getElementById("color").innerHTML = data.color;
+(function product_info(){
+  const product = `
+  <div class="w-full h-fit mb-5">
+    <h6 class="leading-7 uppercase text-base mb-3">The fit</h6>
+    <p class="leading-7 text-base font-bold">${data.title}</p>
+    <p class="leading-7 text-lg font-normal">- ${data.description}</p>
+  </div>`
+
+  document.getElementById("product-info").innerHTML = product;
+})();
+
+
+// (function(){
+//   const product = `
+//   <div class="flex flex-col lg:flex-row w-full h-fit border-t-1 py-2">
+//     <div class="w-fit lg:w-[20%]">
+//       <h6 class="uppercase text-sm leading-6">Material</h6>
+//     </div>
+//     <div>
+//       <p class="text-sm leading-6 uppercase leading-6">${data.material}</p>
+//     </div>
+//   </div>
+//   <div class="flex flex-col lg:flex-row w-full h-fit border-t-1 py-2">
+//     <div class="w-fit lg:w-[20%]">
+//       <h6 class="uppercase text-sm leading-6">Made in</h6>
+//     </div>
+//     <div>
+//       <p class="text-sm leading-6 uppercase leading-6">${data.made_in}</p>
+//     </div>
+//   </div>
+//   <div class="flex flex-col lg:flex-row w-full h-fit border-t-1 py-2">
+//     <div class="w-fit lg:w-[20%]">
+//       <h6 class="uppercase text-sm leading-6">Product code</h6>
+//     </div>
+//     <div>
+//       <p class="text-sm leading-6 uppercase leading-6">${data.product_code}</p>
+//     </div>
+//   </div>
+//   <div class="flex flex-col lg:flex-row w-full h-fit border-t-1 py-2">
+//     <div class="w-fit lg:w-[20%]">
+//       <h6 class="uppercase text-sm leading-6">services</h6>
+//     </div>
+//     <div>
+//       <p class="text-sm leading-6 uppercase leading-6">${data.product_code}</p>
+//     </div>
+//   </div>
+//   <div class="w-full h-fit my-2 border-t-1 py-2">
+//     <button class="uppercase text-sm py-3 hover:bg-gray-200"><a href="#">Read more</a></button>
+//   </div>
+//   `
+//   document.getElementById("product-information").innerHTML = product;
+// })();
+
+/*<div class="flex flex-col lg:flex-row w-full h-fit border-t-1 py-2">
+  <div class="w-fit lg:w-[20%]">
+    <h6 class="uppercase text-sm leading-6">Material</h6>
+  </div>
+  <div>
+    <p class="text-sm leading-6 leading-6">100% Linen Lining: 100% Viscose</p>
+  </div>
+</div>*/
+
+document.getElementById("title").innerText = data.title;
+document.getElementById("price").innerText = `$${data.price}.00`;
+document.getElementById("color").innerText = data.color;
 document.getElementById("img-1").src = data.images[0];
 document.getElementById("img-2").src = data.images[1];
 
@@ -26,8 +89,6 @@ async function get_data() {
 
   return data;
 }
-
-map_image(data);
 
 function map_image(data) {
   const img_list = data.images.map((el, i) => {
