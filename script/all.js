8a15755e1e440edbaa92b790e417afde91e530e5
BRajendra10 <rajendrabehera8116@gmail.com>
2025-06-07 09:34:48 +0530

    commits

--- script/all.js
+++ script/all.js
@@ -3,12 +3,12 @@ const help_modal = document.getElementById("help-modal");
 const login_modal = document.getElementById("login-modal");
 const bag_modal = document.getElementById("bag-modal");
 const usd_modal = document.getElementById("usd-modal");
-
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach((btn, i) => {
   btn.addEventListener("click", () => {
     let target = btn.classList;
+    
     if (target.contains("help-btn")) {
       help_modal.classList.remove("hidden");
     } else if (target.contains("help-modal-close")) {
@@ -24,6 +24,5 @@ btns.forEach((btn, i) => {
     } else {
       console.log(target);
     }
-    // console.log(btn.classList);
   });
 });
